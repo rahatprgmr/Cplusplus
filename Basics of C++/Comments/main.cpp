b31bb8435c1b925d623dd44ffed53bb9e3d87777
rahatdev99 <rahatdev99@gmail.com>
2023-01-24 23:30:03 +0600

    comments

--- Basics of C++/Comments/main.cpp
+++ Basics of C++/Comments/main.cpp
@@ -6,5 +6,8 @@
 int main() {
     std::cout<<"Hello world!";//print Hello world.
     std::cout<<"I'am c++ program!";//print I'am c++ program.
+    //C++ supports two ways of commenting:
+    //line comments
+    /*block comments*/
     return 0;
 }
