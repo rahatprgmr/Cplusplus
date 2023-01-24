37a6d4713dd547879200dd409f6362424a281d8c
rahatdev99 <rahatdev99@gmail.com>
2023-01-24 22:10:04 +0600

    comments

---
+++ Basics of C++/Comments/main.cpp
@@ -0,0 +1,10 @@
+/*
+  My program is c++ with more comments.
+*/
+#include <iostream>
+
+int main() {
+    std::cout<<"Hello world!";//print Hello world.
+    std::cout<<"I'am c++ program!";//print I'am c++ program.
+    return 0;
+}
---
+++ Basics of C++/Structure of a program/main.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+// int main() {
+//     std::cout<<"Hello world! ";
+//     std::cout<<"I'am c++ program!";
+//     return 0;
+// }
+
+// int main() {std::cout<<"Hello world! ";std::cout<<"I'am c++ program!";return 0;}
+
+int main() 
+{
+    std::cout<<
+      "Hello world! ";
+    std::cout
+       <<"I'am c++ program!";  
+}
+
