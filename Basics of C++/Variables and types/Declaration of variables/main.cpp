01c95b68f849a328b3b309e873a895c0b9eb8401
rahatdev99 <rahatdev99@gmail.com>
2023-01-26 23:28:25 +0600

    Declaration of variables

---
+++ Basics of C++/Variables and types/Declaration of variables/main.cpp
@@ -0,0 +1,29 @@
+//operation with variables
+#include <iostream>
+
+int main() {
+    //declaration of variables
+    /*
+    int a;
+    float myNumber;
+
+    int a,b,c,d;
+    int a;
+    int b;
+    int c;
+    int d;
+    */
+
+    //declaration variable
+    int a,b;
+    int result;
+    //process
+    a = 5;
+    b = 2;
+    a = a+1;
+    result = a-b;
+    //print out the result
+    std::cout<<result<<std::endl;
+    // terminate the program:
+    return 0;
+}
