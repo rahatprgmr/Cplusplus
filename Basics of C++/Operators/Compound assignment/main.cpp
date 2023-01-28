14825f640800a17c1d6c67398d0b9fba8be820db
rahatdev99 <rahatdev99@gmail.com>
2023-01-28 21:51:38 +0600

    compound assignment operators

---
+++ Basics of C++/Operators/Compound assignment/main.cpp
@@ -0,0 +1,15 @@
+// compound assignment operators
+#include <iostream>
+using namespace std;
+
+int main() {
+    //Compound assignment (+=, -=, *=, /=, %=, >>=, <<=, &=, ^=, |=)
+
+    int a,b = 3;
+    
+    a = b;
+    a += 2;
+    
+    cout<<a<<endl;
+    return 0;
+}
