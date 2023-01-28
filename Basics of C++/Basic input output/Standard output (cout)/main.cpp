82a58bcc2fae10909b3f6ec9d76e66c6e7e942aa
rahatdev99 <rahatdev99@gmail.com>
2023-01-29 00:22:05 +0600

    standard output (cout)

---
+++ Basics of C++/Basic input output/Standard output (cout)/main.cpp
@@ -0,0 +1,28 @@
+//standard output (cout)
+#include <iostream>
+using namespace std;
+
+int main() {
+
+    //Standard output (cout)
+
+    cout << "Output sentence"; // prints Output sentence on screen
+    cout << 120;               // prints number 120 on screen
+
+    cout << "Hello";  // prints Hello
+
+    int age = 18;
+    int zipcode = 23244;
+
+    cout << "I am " << age << " years old and my zipcode is " << zipcode<<endl;
+
+    //Alternatively, the endl manipulator can also be used to break lines. For example:
+
+    cout << "First sentence." << endl;
+    cout << "Second sentence." << endl;
+
+
+
+
+    return 0;
+}
