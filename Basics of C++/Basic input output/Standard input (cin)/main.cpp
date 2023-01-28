7248462ac3e370394ebad6632af02b0c03d8b418
rahatdev99 <rahatdev99@gmail.com>
2023-01-29 00:38:31 +0600

    standard input (cin)

---
+++ Basics of C++/Basic input output/Standard input (cin)/main.cpp
@@ -0,0 +1,28 @@
+//standard input (cin)
+#include <iostream>
+using namespace std;
+
+int main() {
+
+    //Standard input (cin)
+
+    int age;
+    cin>>age;
+
+    cout<<"age is: "<<age<<endl;
+
+    int i;
+    cout << "Please enter an integer value: ";
+    cin >> i;
+    cout << "The value you entered is " << i;
+    cout << " and its double is " << i*2 << ".\n";
+
+    int a,b;
+
+    cin>>a>>b;
+
+    cout<<a<<endl;
+    cout<<b<<endl;
+
+    return 0;
+}
