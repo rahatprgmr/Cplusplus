2e5181834ce5b07689c1939f28a971371e55b8fe
rahatdev99 <rahatdev99@gmail.com>
2023-01-28 21:39:56 +0600

    Arithmetic operators

---
+++ Basics of C++/Operators/Arithmetic operators/main.cpp
@@ -0,0 +1,32 @@
+//arithmetic operators
+#include <iostream>
+using namespace std;
+
+int main() {
+    //Arithmetic operators
+    //The five arithmetic operators supported by c++ are:
+
+    /*
+       + --> addition.
+       - --> subtraction.
+       * --> multiplication
+       / --> division.
+       % --> modulo.
+    */
+
+     int a,b,c,d,e;
+     a = 10+20;
+     b = a - 10;
+     c = b * 5;
+     d = c / a;
+     e = a % b;
+
+     cout<<"a = "<<a<<endl;
+     cout<<"b = "<<b<<endl;
+     cout<<"c = "<<c<<endl;
+     cout<<"d = "<<d<<endl;
+     cout<<"e = "<<e<<endl;
+
+
+    return 0;
+}
