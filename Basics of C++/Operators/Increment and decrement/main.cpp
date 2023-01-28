5ebd4f3b5655005de34e1381e3737a181175be52
rahatdev99 <rahatdev99@gmail.com>
2023-01-28 22:03:56 +0600

    increment and decrement

---
+++ Basics of C++/Operators/Increment and decrement/main.cpp
@@ -0,0 +1,20 @@
+//increment and decrement (++, --)
+#include <iostream>
+using namespace std;
+
+int main() {
+    //increment and decrement (++, --)
+
+    int a, b;
+    a = 5;
+    b = 4;
+    a++;
+    ++a;
+    b++;
+    ++b;
+
+    cout<<a<<endl;
+    cout<<b<<endl;
+
+    return 0;
+}
