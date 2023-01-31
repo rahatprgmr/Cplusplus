c5adc907fb0f3055d45d28aa8cc00531b40db5d9
rahatdev99 <rahatdev99@gmail.com>
2023-01-31 19:26:00 +0600

    Arguments passed by value and by reference

--- Program structure/Functions/Arguments passed by value and by reference/main.cpp
+++ Program structure/Functions/Arguments passed by value and by reference/main.cpp
@@ -10,6 +10,9 @@ void duplicate(int& a,int& b,int& c) {
 }
 
 int main() {
+    /*
+      In the functions seen earlier, arguments have always been passed by value. This means that, when calling a function, what is passed to the function are the values of these arguments on the moment of the call, which are copied into the variables represented by the function parameters. For example, take:
+    */
     int x = 1, y = 3, z = 7;
      duplicate (x, y, z);
      cout << "x=" << x << ", y=" << y << ", z=" << z<<endl;
